247de433c1a58a361dc16496582b7bb84b718295
Dax Ravi <d@ironbay.co>
2021-09-15 22:19:02 -0400

    Add not supported error

--- src/api/error.ts
+++ src/api/error.ts
@@ -7,6 +7,7 @@ type GenericErrorOpts = {
   title?: string
   detail?: string
 }
+
 export class GenericError extends Error {
   public opts: GenericErrorOpts
   constructor(opts: GenericErrorOpts) {
@@ -14,12 +15,23 @@ export class GenericError extends Error {
     this.opts = opts
   }
 }
+
 export class NotFoundError extends GenericError {
   constructor() {
     super({
       code: "not_found",
       status: 404,
       detail: "This resource was not found",
+    })
+  }
+}
+
+export class NotSupported extends GenericError {
+  constructor() {
+    super({
+      code: "not_supported",
+      status: 400,
+      detail: "This endpoint is not supported",
     })
   }
 }
